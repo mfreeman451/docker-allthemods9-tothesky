10985c3331a1a7cf4154c7a648cc36c03c84eb73
Michael Freeman <mfreeman451@gmail.com>
2024-05-30 16:40:28 -0500

    🔧 sync

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,15 @@
 # syntax=docker/dockerfile:1
 
+<<<<<<< HEAD
 FROM --platform=linux/amd64 openjdk:17-buster
+=======
+FROM --platform=linux/amd64 ubuntu:20.04
+>>>>>>> c8453f9 (🔧 sync)
 
 LABEL version="0.2.60"
 
-RUN apt-get update && apt-get install -y curl unzip && \
+# Install OpenJDK, curl, and unzip
+RUN apt-get update && apt-get install -y openjdk-17-jdk curl unzip && \
  adduser --uid 99 --gid 100 --home /data --disabled-password minecraft
 
 COPY launch.sh /launch.sh
--- launch.sh
+++ launch.sh
@@ -12,10 +12,10 @@ else
 	exit 99
 fi
 
-if ! [[ -f 'Server-Files-0.2.60.zip' ]]; then
+if ! [[ -f 'server-1.0.zip' ]]; then
 	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
-	curl -Lo 'Server-Files-0.2.60.zip' 'https://edge.forgecdn.net/files/5378/746/Server-Files-0.2.60.zip' || exit 9
-	unzip -u -o 'Server-Files-0.2.60.zip' -d /data
+    curl -Lo 'server-1.0.zip' 'https://mediafilez.forgecdn.net/files/5384/734/server-1.0.zip' || exit 9
+	unzip -u -o 'server-1.0.zip' -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
 		cd "${DIR_TEST}"
